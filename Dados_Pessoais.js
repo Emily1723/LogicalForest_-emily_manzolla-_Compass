84bc4bdb277219c4550cab67f78a0a213c4e6e30
Emily Kaylane Manzolla de Souza Bertoldo <emily.bertoldo@compasso.com.br>
2024-04-19 13:58:51 -0300

    adicionado linha teste

---
+++ Dados_Pessoais.js
@@ -0,0 +1,20 @@
+// let idade = 5;
+// console.log(idade);
+//let altura = 180;
+// camelCase
+//teste
+let nome = 'jhonatan';
+console.log(nome);
+let idade = 25;
+let estaAprovado = true;
+let sobrenome ="silva";
+ 
+ 
+let pessoa = {
+    nome:'Rafael'
+ 
+};
+ 
+console.log(pessoa);
+console.log (Rafael);
+console.log(sobrenome);
